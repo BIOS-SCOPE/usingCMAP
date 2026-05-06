ed360e0e30efdf5f7d6326353df13191563a7610
redbluewater <klongnecker@whoi.edu>
2026-05-05 21:42:56 -0700

    add exmaple config

---
+++ my_config_EXAMPLE.py
@@ -0,0 +1,6 @@
+
+## Register at https://simonscmap.com/ and get an API_KEY.
+## Plug in your API_KEY below.
+
+#KL make sure this is in a place behind .gitignore
+API_KEY = "SOME LONG NUMBER FROM CMAP"
